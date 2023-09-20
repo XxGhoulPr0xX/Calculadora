43ee23df4508e0a84a0ac3d9b2197e9752ac5e3c
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-09-20 21:57:05 +0200

    🔧 chore: add eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+// @ts-check
+
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+    $schema: 'http://json.schemastore.org/eslintrc',
+    root: true,
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    parser: '@typescript-eslint/parser',
+    extends: ['plugin:astro/recommended', 'plugin:astro/jsx-a11y-recommended'],
+    overrides: [
+        {
+            files: ['*.astro'],
+            parser: 'astro-eslint-parser',
+            parserOptions: {
+                parser: '@typescript-eslint/parser',
+                extraFileExtensions: ['.astro'],
+            },
+            rules: {},
+        },
+        {
+            // ensures to also lint these extensions
+            files: ['*.cjs', '*.mjs', '*.ts'],
+        },
+    ],
+    // improves performance by ignoring node_modules
+    ignorePatterns: ['node_modules', 'bun.lockb'],
+}
