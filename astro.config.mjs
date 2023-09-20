6c6389fd7907b3f509f20bda59716f0c45bc9cce
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-09-20 22:18:33 +0200

    ✨ feat: integrate react into astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
+import react from '@astrojs/react'
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
-    build: { assets: 'assets' },
+    integrations: [tailwind(), react()],
+    build: {
+        assets: 'assets',
+    },
 })
