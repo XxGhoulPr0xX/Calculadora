e78c80bd3f2f3a8843b84a113e4c6ea02763aac8
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-09-20 22:07:23 +0200

    🔧 chore: remove declarations files

--- src/declarations/calculator/calculator.did.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Principal } from '@dfinity/principal';
-import type { ActorMethod } from '@dfinity/agent';
-
-export interface _SERVICE {
-  'Division' : ActorMethod<[bigint, bigint], string>,
-  'Multiplicacion' : ActorMethod<[bigint, bigint], bigint>,
-  'Resta' : ActorMethod<[bigint, bigint], bigint>,
-  'Suma' : ActorMethod<[bigint, bigint], bigint>,
-}
--- src/declarations/calculator/calculator.did.js
+++
@@ -1,9 +0,0 @@
-export const idlFactory = ({ IDL }) => {
-  return IDL.Service({
-    'Division' : IDL.Func([IDL.Nat, IDL.Nat], [IDL.Text], []),
-    'Multiplicacion' : IDL.Func([IDL.Nat, IDL.Nat], [IDL.Nat], []),
-    'Resta' : IDL.Func([IDL.Nat, IDL.Nat], [IDL.Nat], []),
-    'Suma' : IDL.Func([IDL.Nat, IDL.Nat], [IDL.Nat], []),
-  });
-};
-export const init = ({ IDL }) => { return []; };
